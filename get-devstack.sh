0466ddf1bd8399d8b85fbcccf3db6328a94807a5
Steph DGenius <stephdgenius@steph-dgenius-MacBook-Pro.local>
2019-04-11 22:49:17 +0100

    :sparkles: Update devstack

--- get-devstack.sh
+++ get-devstack.sh
@@ -5,3 +5,6 @@ clear
 echo "Downloading devstack..."
 cd /
 sudo git clone https://git.openstack.org/openstack-dev/devstack
+
+# Copie du fichier de configuration de DevStack
+sudo cp /devstack-setup/config/local.conf /devstack/local.conf
