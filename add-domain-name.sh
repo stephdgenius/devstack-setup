eb917753b8f15e85ae7da79a8431717c90636671
Steph DGenius <stephdgenius@steph-dgenius-MacBook-Pro.local>
2019-04-11 15:03:59 +0100

    :sparkles: Add new script and update swift setup

---
+++ add-domain-name.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+
+# Copie du fichier de configuration de DevStack
+sudo cp /media/usb/config/local.conf /devstack/local.conf
--- swift-st-setup-1.sh
+++ swift-st-setup-1.sh
@@ -9,3 +9,5 @@ sudo mkfs.xfs -i size=1024 -s size=4096 /dev/sdb
 mkdir -p /srv/node/device0
 mount -o noatime,nodiratime,nobarrier /dev/sdb /srv/node/device0
 chown -R swift. /srv/node
+sudo cp /media/usb/config/swift/fstab /etc/fstab
+sudo nano /etc/fstab
